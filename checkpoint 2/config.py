acf94a8e93b7971a8a51cd0e5219c6cd4d91bde0
eric157 <ericpeterthomas15@gmail.com>
2025-01-27 03:26:28 +0530

    commit04

--- checkpoint 2/config.py
+++
@@ -1,6 +0,0 @@
-CHECKPOINT_1_OUTPUT_DIR = "../checkpoint 1/output"
-MODEL_DIR = "models"
-OUTPUT_DIR = "output"
-PREPROCESSED_DATA_FILE = "FakeReviewDataPreprocessed.csv"
-METRICS_FILE = "model_evaluation_metrics.txt"
-RANDOM_STATE = 42
